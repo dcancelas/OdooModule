f808081e516948e4c3d383fecaaec3baf0365bb2
dcancelas <dcancelasgarcia@danielcastelao.org>
2021-03-21 19:31:19 +0100

    Modificaciones en manifest y openacademy

--- addons/academy/__manifest__.py
+++ addons/academy/__manifest__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "academy",
+    'name': "openacademy",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Openacademy tutorial""",
 
     'description': """
-        Long description of module's purpose
+        Odoo module made following the open academy tutorial 
     """,
 
     'author': "My Company",
@@ -16,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Education',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -27,6 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/openacademy.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
